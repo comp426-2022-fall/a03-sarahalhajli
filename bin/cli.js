e9979d9e3070613b9ead6b6558890f41d1bcad88
Sarah Alhajli <sarahalh@live.unc.edu>
2022-11-22 11:36:17 -0500

    Added info in cli and roll

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { roll } from "/lib/roll.js";
+import minimist from 'minimist';
+
+const args = minimist(process.argv.slice(2));
+
+var sides = args.sides;
+var dice = args.dice;
+var rolls = args.rolls;
+
+var results = roll(sides, dice, rolls);
+
+console.log(JSON.stringify({ sides: sides, dice: dice, rolls: rolls, results: results }));
