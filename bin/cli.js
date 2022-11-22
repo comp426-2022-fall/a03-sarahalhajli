7398428ba8d3ed0d9500d7ba4a960ba1ffb80215
Sarah Alhajli <sarahalh@live.unc.edu>
2022-11-22 12:31:37 -0500

    Getting rolls to display

--- bin/cli.js
+++ bin/cli.js
@@ -8,8 +8,18 @@ var sides = args.sides;
 var dice = args.dice;
 var rolls = args.rolls;
 
+if(args.sides == "") {
+	sides = 6;
+}
+if(args.dice == "") {
+	dice = 2;
+}
+if(args.rolls == "") {
+	rolls = 1;
+}
+
 var results = roll(sides, dice, rolls);
 
-console.log(JSON.stringify({results}));
+console.log(JSON.stringify({sides: sides, dice: dice, rolls: rolls, results: results}));
 
 process.exit(0);
