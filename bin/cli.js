206a2b0021008bfc62bf369841d3279cd372fb66
Sarah Alhajli <sarahalh@live.unc.edu>
2022-11-22 11:56:01 -0500

    Fixed import error

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { roll } from "/lib/roll.js";
+import { roll } from "../lib/roll.js";
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
@@ -10,4 +10,6 @@ var rolls = args.rolls;
 
 var results = roll(sides, dice, rolls);
 
-console.log(JSON.stringify({ sides: sides, dice: dice, rolls: rolls, results: results }));
+console.log(JSON.stringify({results}));
+
+process.exit(0);
